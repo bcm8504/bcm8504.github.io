da5eeee5953ef4da51621cba9e135c92cd914831
Brandon Montijo <bcm8504@rit.edu>
2024-10-29 17:32:33 -0500

    Remove vue-app

--- vue-app/src/pages/Cooking.tsx
+++
@@ -1,9 +0,0 @@
-const Cooking = () => {
-    return (
-        <div className={'container'}>
-            Cooking
-        </div>
-    )
-}
-
-export default Cooking;
--- vue-app/src/pages/Music.tsx
+++
@@ -1,9 +0,0 @@
-const Music = () => {
-    return (
-        <div className={'container'}>
-            Music
-        </div>
-    )
-}
-
-export default Music;
